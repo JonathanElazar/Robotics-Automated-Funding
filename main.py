2096b45ae016796155749f1aa1db5bee2beded6b
JonathanElazar <jonathanelazar89@gmail.com>
2026-04-21 15:04:56 -0500

    Fix string splitting for sponsors lists

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ with open("config/possible-sponsors.txt", "r") as f:
 with open("data/email-sponsors.txt", "r") as f:
     emailed_sponsors = f.read()
 
-possible_sponsors = possible_sponsors("\n")
-emailed_sponsors = emailed_sponsors.read("\n")
+possible_sponsors = possible_sponsors.split("\n")
+emailed_sponsors = emailed_sponsors.split("\n")
 
 for i in range(len(possible_sponsors)):
     
