b780b55d70a212fdc5c1b10b9f6762dafcd0ebab
JonathanElazar <jonathanelazar89@gmail.com>
2026-04-21 15:08:49 -0500

    Filter possible sponsors not yet emailed

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ with open("data/email-sponsors.txt", "r") as f:
 
 possible_sponsors = possible_sponsors.split("\n")
 emailed_sponsors = emailed_sponsors.split("\n")
+email_list = []
 
 for i in range(len(possible_sponsors)):
-    
+    if possible_sponsors[i] not in emailed_sponsors:
+        email_list.append(possible_sponsors[i])
