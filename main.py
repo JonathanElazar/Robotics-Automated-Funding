b1972222c4540ea0389a981ab890db74ec5ac300
JonathanElazar <jonathanelazar89@gmail.com>
2026-04-21 15:25:07 -0500

    Create email list and verify it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 with open("config/possible-sponsors.txt", "r") as f:
     possible_sponsors = f.read()
 
@@ -11,3 +13,16 @@ email_list = []
 for i in range(len(possible_sponsors)):
     if possible_sponsors[i] not in emailed_sponsors:
         email_list.append(possible_sponsors[i])
+
+print("Sending emails to the following sponsors: ")
+print(email_list)
+
+while True:
+    confirm = input("Confirm this is ok to continue (Y/n): ")
+    if confirm == "Y" or confirm == "y":
+        break
+    elif confirm == "N" or confirm == "n":
+        print("Exiting")
+        sys.exit(0)
+
+
